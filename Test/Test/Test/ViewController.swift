5dd44d4120760a3311d7b287752625e993746153
sevaktadevosyan <sevak.tadevosyan@picsart.com>
2022-11-03 10:48:32 +0400

    prj

---
+++ Test/Test/Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Sevak Tadevosyan on 02.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
